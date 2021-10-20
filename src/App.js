f31d6bec08564bf2e66c574c005368b4dcffca16
Oren Zion <orenzion89@gmail.com>
2021-10-20 15:28:18 +0300

    stoped

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <h1>Hello World</h1>
     </div>
+    </Router>
   );
 }
 
